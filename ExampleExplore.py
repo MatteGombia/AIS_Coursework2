b82b56b0af2343cd89e986d1b1373c37d71e10f8
Mjaa <19050037@brookes.ac.uk>
2025-11-19 11:48:23 +0000

    Update ExampleExplore.py

--- ExampleExplore.py
+++ ExampleExplore.py
@@ -1,22 +1,14 @@
 import cozmo
-from cozmo.util import distance_mm, speed_mmps, degrees
-
+import time
+from cozmo.util import degrees
 def cozmo_program(robot: cozmo.robot.Robot):
     while True:
-        front_obstacles = robot.world.visible_objects
-        obstacle_close = False
-        
-        for obj in front_obstacles:
-            if obj.pose.position.x > 0 and obj.pose.position.x < 150:
-                obstacle_close = True
-                break
-        
-        if obstacle_close:
-            robot.drive_wheels(0, 0)
-            robot.turn_in_place(degrees(90)).wait_for_completed()
+        robot.drive_wheels(50, 50)
+        time.sleep(0.5)
+        if robot.is_moving:
+            continue
         else:
-            robot.drive_wheels(50, 50)
-        
-        robot.sleep(0.1)
-
+            robot.drive_wheels(-50, -50, duration=0.5)
+            robot.turn_in_place(degrees(90)).wait_for_completed()
+            time.sleep(0.1)
 cozmo.run_program(cozmo_program)
