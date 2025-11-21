e88a2f755f17c577928cb38c43f7be758fa1ef6a
Mjaa <19050037@brookes.ac.uk>
2025-11-21 15:31:29 +0000

    Update ExploreCamera.py

--- ExploreCamera.py
+++ ExploreCamera.py
@@ -3,7 +3,7 @@ import time
 from cozmo.util import degrees
 import numpy as np
 
-def cozmo_program(robot: cozmo.robot.Robot):
+def explorewithcamera(robot: cozmo.robot.Robot):
     robot.camera.image_stream_enabled = True
     while True:
         img1 = robot.world.latest_image
@@ -25,11 +25,11 @@ def cozmo_program(robot: cozmo.robot.Robot):
         movement_score = np.mean(diff)
         print(f"Image difference: {movement_score:.2f}")
         if movement_score <5:
-            print("No visual movement - likely stuck")
+            print("No visual movement detected")
             robot.drive_wheels(-150, -150)
             time.sleep(1.0)
             robot.drive_wheels(0, 0)
             robot.turn_in_place(degrees(90)).wait_for_completed()
         else:
             print("Visual movement detected")
-cozmo.run_program(cozmo_program)
+cozmo.run_program(explorewithcamera)
